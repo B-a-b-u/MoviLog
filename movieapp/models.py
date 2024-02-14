94619ea7229951d5da4aa2f31b387ab7fb2b702f
“Baburaj” <sbrbabu123@gmail.com>
2024-02-14 09:49:55 +0530

    Created new model for comment and Added Likes field on post model

--- movieapp/models.py
+++ movieapp/models.py
@@ -8,6 +8,20 @@ class Post(models.Model):
     email = models.EmailField()
     caption = models.CharField(max_length=50)
     content = models.TextField()
+    likes= models.IntegerField(default = 0)
+
 
     def __str__(self):
         return self.caption[:]
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete = models.CASCADE,related_name = "comments")
+    commenter_name = models.CharField(max_length = 30)
+    comment = models.TextField(max_length = 150)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.created} {self.commenter_name} : {self.comment}"
+    
+    def delete_comment(self):
+        self.delete()
